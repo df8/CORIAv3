ddfdfd40bdb8670e3133e3122cd77eb2b3534367
Sebastian Gross <bigbasti@gmail.com>
2017-05-16 18:16:52 +0200

    AngularJS up and running

---
+++ coria-web/src/main/webapp/public/app/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/*global angular */
+/*global alert */
+
+angular.module('app.services', []);
+angular.module('app.components', ['ngResource']);
+
+var app = angular.module('coria', [
+    'ngSanitize'
+    , 'ngAria'
+    , 'ngRoute'
+    , 'ngMessages'
+    , 'ui.bootstrap'
+    , 'ngResource'
+    , 'app.components'
+    // , 'app.directives'
+    // , 'app.filters'
+    // , 'app.services'
+    // , 'fmTimepicker'
+//    , 'ngDialog'
+//    , 'ngCookies'
+]);
+
+var mod = app
+    .config ( ['$routeProvider', '$httpProvider',
+        function ($routeProvider, $httpProvider) {
+            $routeProvider
+                .when('/', { redirectTo: '/login' })
+                .when('/users', { templateUrl: 'views/users/users.html' })
+                .when('/locks', { templateUrl: 'views/locks/locks.html' })
+                .otherwise({ template: "<h2>Not yet implemented</h2>"});
+        }]);
+
+mod.factory('myHttpInterceptor', ['$rootScope', '$q', '$location', '$injector', 'errorDataService', 'globalSettings', '$timeout',
+    function ($rootScope,   $q,   $location,   $injector,   errorDataService,   globalSettings,   $timeout) {
+        var delayWaitAn;
+        return {
+
+        };
+    }]);
+
+mod.config(['$httpProvider', function ($httpProvider) {
+    //$httpProvider.interceptors.push('myHttpInterceptor');
+}]);
+
+
+mod.config(['$httpProvider', function($httpProvider) {
+    //initialize get if not there
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};
+    }
+
+    //disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    // extra
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
